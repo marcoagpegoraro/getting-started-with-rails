fd7617e7c6df1aad7cd004ff51a638e2ebe6d74a
Marco Antonio Gonçalves Pegoraro <marco@astolfo-chan.localdomain>
2020-07-19 22:08:16 -0300

    cr part of the crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,30 @@
 class ArticlesController < ApplicationController
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
     
-    def show
-        @article = Article.find(params[:id])
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # @article = Article.new(params[:article]) n pode pq é inseguro
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+    # redirect_to @article
+    # render plain: params[:article].inspect
+  end
 
-    def new
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
     end
-
-    def create
-        # @article = Article.new(params[:article]) n pode pq é inseguro
-
-        @article = Article.new(article_params)
-
-        @article.save
-
-        redirect_to @article
-        # render plain: params[:article].inspect
-    end
-
-    private
-        def article_params
-            params.require(:article).permit(:title, :text)
-        end
 end
